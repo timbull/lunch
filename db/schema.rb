6ba178c16dbe7b6ee1801765655ff2f23e59dbd5
Tim Bull <tim@avos.com>
2012-04-08 20:27:47 -0700

    Added a migration to include Active on the people and updated the forms in the scaffolding to show this correctly.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120408224521) do
+ActiveRecord::Schema.define(:version => 20120409031346) do
 
   create_table "people", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+    t.boolean  "active",     :default => true
   end
 
 end
