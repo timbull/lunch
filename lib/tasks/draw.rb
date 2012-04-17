e21e094fec6f391e1e0334323cd64e8a0623fd57
Tim Bull <tim@avos.com>
2012-04-17 16:40:55 -0700

    First successful "draw" implemented.  Now to lay it out then move on to saving past draws and so forth to implement a better algorithm.

--- lib/tasks/draw.rb
+++ lib/tasks/draw.rb
@@ -3,13 +3,32 @@ class Draw
   def initialize
     @draw_people = Array.new
 
-    Person.find_each do |person|
+    Person.where("active=True").find_each do |person|
       @draw_people.push(person)
     end
   end
 
   def generate
-    @draw_people
+    # Do the draw!
+    @new_draw = Array.new
+    r = Random.new
+    draw_pool = @draw_people
+    num_draws = draw_pool.length
+
+    num_draws.times do |i|
+      # Generate a random number within the array space.
+      if draw_pool.length == 0
+        break
+      end
+      person_drawn = r.rand(1..draw_pool.length) - 1
+
+      #Move the person drawn from the people to the new_draw
+      @new_draw.push(draw_pool[person_drawn])
+      draw_pool.delete_at(person_drawn)
+    end
+
+    #Return the new draw
+    @new_draw
   end
 
 end
